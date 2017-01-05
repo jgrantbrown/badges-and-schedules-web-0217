ff20055abe2fa196a3105c1e3678443336efdc97
jason brown <j.grant.brown@gmail.com>
2017-01-05 04:51:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badge_maker)
+attendees_list= []
+ return attendees_list.push(badge_maker(name))
+end
+
+def assign_rooms(attendees)
+  return "#{badge_maker} "
+
+end
+
+def printer
+  print "#{assign_rooms}"
+end
